e1dc517a3ccfec81bad7af2aa1ec060f6770d070
Alejandro Vicente Grabovetsky <a.vicente.grab@gmail.com>
2017-11-04 19:16:54 +0000

    Added examples to documentation

--- index.js
+++ index.js
@@ -2,11 +2,34 @@ const gm = require('gm');
 const debug = require('debug')('gm-base64');
 
 /**
-* Convert an image to a Base64 image.
-*
-* @param {boolean} toDataUri - Whether to convert to a Data URI or not.
-* @param {function} callback - Callback to run after conversion succeeds.
-*/
+ * Convert an image to a Base64 image.
+ *
+ * @example
+ * // With a Data URI
+ * const gm = require('gm');
+ * require('gm-base64');
+ *
+ * let toDataUri = true;
+ * gm('a.jpg')
+ *   .resize(384)
+ *   .toBase64(toDataUri, function(err, base64){
+ *     console.log(base64);
+ *   });
+ *
+ * @example
+ * // Without a Data URI
+ * const gm = require('gm');
+ * require('gm-base64');
+ *
+ * gm('a.jpg')
+ *   .resize(384)
+ *   .toBase64(function(err, base64){
+ *     console.log(base64);
+ *   });
+ *
+ * @param {boolean} toDataUri - Whether to convert to a Data URI or not.
+ * @param {function} callback - Callback to run after conversion succeeds.
+ */
 gm.prototype.toBase64 = function(toDataUri, callback) {
   if (typeof toDataUri === 'function') {
     callback = toDataUri;
